416962fa4e633e2981e359fa56e7b77adaeb68be
Mostafa Said <39448733+sunhorus@users.noreply.github.com>
2022-01-13 16:58:48 +0200

    Delete dist directory

--- dist/helpers/usersAccess.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UsersStoreAccess {
-    signIn() {
-        throw new Error("Method not implemented.");
-    }
-}
-exports.default = UsersStoreAccess;
